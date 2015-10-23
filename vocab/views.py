6a799f9a8fd04a089972d6abb13058d1c961bd2a
M. Monster <horatius83@gmail.com>
2015-10-23 12:49:34 -0500

    Added quiz view

--- vocab/views.py
+++ vocab/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the vocab index")
+    template = loader.get_template('vocab/index.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
+
+def quiz(request):
+    template = loader.get_template('vocab/quiz.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
 
 
--- vocabsite/urls.py
+++ vocabsite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^vocab/', include('vocab.urls')),
+    url(r'^quiz/', include('vocab.urls')),
 ]
